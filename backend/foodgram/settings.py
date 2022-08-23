b1d2843a26dbe2e2921d8f0d1ce14c91ff492779
Екатерина Серова <katyaserova@yandex.ru>
2022-08-23 22:00:13 +0300

    deploy CI CD

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         ),
         "NAME": os.getenv("DB_NAME", default="postgres"),
         "USER": os.getenv("POSTGRES_USER", default="postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
-        "HOST": os.getenv("DB_HOST", default="foodgram_db"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="12345"),
+        "HOST": os.getenv("DB_HOST", default="db"),
         "PORT": os.getenv("DB_PORT", default=5432),
     }
 }
